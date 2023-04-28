75f7b70c48614c17f9ecdbfc2b7bead9a45add05
rjzarate <115201416+rjzarate@users.noreply.github.com>
2023-04-28 11:29:57 -0700

    Create 1773. Count Items Matching a Rule.java

---
+++ 1773. Count Items Matching a Rule.java
@@ -0,0 +1,34 @@
+/*
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+https://leetcode.com/problems/count-items-matching-a-rule/
+*/
+
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 0;
+        switch (ruleKey) {
+            case "type":
+                index = 0;
+                break;
+            case "color":
+                index = 1;
+                break;
+            case "name":
+                index = 2;
+                break;
+        }
+        int count = 0;
+        for (List<String> item: items) {
+            count += (item.get(index).equals(ruleValue)) ? 1 : 0;
+        }
+        return count;
+    }
+}
