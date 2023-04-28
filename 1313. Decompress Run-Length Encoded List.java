193ff9e3542367f2a12cca3ef53bd0d045f016ee
rjzarate <115201416+rjzarate@users.noreply.github.com>
2023-04-28 11:23:46 -0700

    Create 1313. Decompress Run-Length Encoded List.java

---
+++ 1313. Decompress Run-Length Encoded List.java
@@ -0,0 +1,27 @@
+/*
+We are given a list nums of integers representing a list compressed with run-length encoding.
+
+Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0).  For each such pair, there are freq elements with value val concatenated in a sublist. Concatenate all the sublists from left to right to generate the decompressed list.
+
+Return the decompressed list.
+
+https://leetcode.com/problems/decompress-run-length-encoded-list/
+*/
+
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        int size = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            size += nums[i];
+        }
+        int[] array = new int[size];
+        int index = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            for (int j = 0; j < nums[i]; j++) {
+                array[index] = nums[i + 1];
+                index ++;
+            }
+        }
+        return array;
+    }
+}
