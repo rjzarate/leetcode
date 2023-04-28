67f31e133bb38a902785aaf9ad39ac2cfb6effb3
rjzarate <115201416+rjzarate@users.noreply.github.com>
2023-04-28 11:33:55 -0700

    Create 2418. Sort the People.java

---
+++ 2418. Sort the People.java
@@ -0,0 +1,29 @@
+/*
+You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+Return names sorted in descending order by the people's heights.
+*/
+
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+        int n = names.length;
+        String[] namesSorted = new String[n];
+        
+
+        for (int i = 0; i < n; i ++) {
+            int tallest = heights[0];
+            int index = 0;
+            for (int j = 0; j < n; j ++) {
+                if (tallest < heights[j]) {
+                    tallest = heights[j];
+                    index = j;
+                }
+            }
+            namesSorted[i] = names[index];
+            heights[index] = -1;
+        }
+        return namesSorted;
+    }
+}
